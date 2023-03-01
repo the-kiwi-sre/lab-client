91c2b511c78f0e64f5df2e327bf1591380f7c787
Stephen Townshend <stownshend@gmail.com>
2023-03-02 11:17:01 +1300

    Tidied up code and swithed to axios library to make API calls

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
-const request = require('request');
+/* This is a demo app which periodically calls a sample service, that's it. In the future it will be extended to have 
+ * Prometheus and OpenTelemetry instrumentation added.
+ */
 
-console.log(process.argv[2])
+// Dependencies
+const axios = require('axios'); // For making HTTP/API requests and handling the response
+const fs = require('fs'); // File server to read our config file
 
-// Call service 1 every 10 seconds
-let myVar = setInterval(function(){ timer() }, 10000);
+// Defines how often to call the downstream service (in seconds)
+const interval = 10;
 
+// Object to hold our configuration
+let configuration;
+
+// Read in JSON config file
+let rawdata = fs.readFileSync('./config.json');
+configuration = JSON.parse(rawdata);
+
+// Creates an interval to execute something every X milliseconds
+let request_interval = setInterval(function(){ timer() }, interval * 1000);
+
+// Code to execute every X seconds
 function timer() 
 {
-    request('http://' + process.argv[2] + '.prometheus-lab.svc.cluster.local:9190/api', { json: true }, (err, res, body) => {
-        //if (err) { return console.log(err); }
-        console.log('We made a request...');
-        console.log('body.json = ' + body.json);
-      });
+    axios.get(configuration.service_url)
+    .then(res => {
+        console.log('INFO: Status Code: ' + res.status + ' and Response Body: ' + JSON.stringify(res.data));
+    })
+    .catch(err => {
+        console.log('Error: ', err.message);
+    });        
 }
 
 function stopFunction() {
-    clearInterval(myVar);
+    clearInterval(request_interval);
 }
 
 
