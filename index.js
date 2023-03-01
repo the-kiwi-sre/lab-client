111ba88e4cbb350c9ae1775f7f90eca9871ac388
Stephen Townshend <stownshend@gmail.com>
2023-03-01 15:31:20 +1300

    Trying to output json

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function timer()
     request('http://' + process.argv[2] + '.prometheus-lab.svc.cluster.local:9190/api', { json: true }, (err, res, body) => {
         //if (err) { return console.log(err); }
         console.log('We made a request...');
-        console.log('body.res = ' + body.res);
+        console.log('body.json = ' + body.json);
       });
 }
 
