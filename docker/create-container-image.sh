615547a7ae6be92a1d8db093c1e4def2a4bb091a
Stephen Townshend <stownshend@gmail.com>
2023-03-01 14:17:11 +1300

    Docker config added

---
+++ docker/create-container-image.sh
@@ -0,0 +1,20 @@
+# Clone the public repo of Datapool Manager
+git clone https://github.com/the-kiwi-sre/lab-client.git
+
+# Copy our Dockerfile into our cloned repo
+cp Dockerfile lab-client/.
+
+# Navigate into the repo folder
+cd lab-client
+
+# Install the required dependencies / modules
+npm install
+
+# Build the Docker image
+docker buildx build --push --platform=linux/amd64 -t thekiwisre/lab-client .
+
+# Head back out to the parent folder
+cd ..
+
+# Delete the folder
+rm -rf datapool-manager
